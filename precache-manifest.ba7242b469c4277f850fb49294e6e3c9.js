992d97f89ae5ba6ba83aa88c9753f8566e030a3a
Avi K <aviavigayl@gmail.com>
2020-07-03 17:34:44 +0300

    Updates

---
+++ precache-manifest.ba7242b469c4277f850fb49294e6e3c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0beefe526b768bb5dd861c5941a4b8e5",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "f54f39b0f3f12962356d",
+    "url": "/minesweeper/static/css/main.07b1e438.chunk.css"
+  },
+  {
+    "revision": "71df3084b409723c6ea8",
+    "url": "/minesweeper/static/js/2.b2540727.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.b2540727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f54f39b0f3f12962356d",
+    "url": "/minesweeper/static/js/main.672e637a.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  },
+  {
+    "revision": "621861ba51dd236623b938cfc21916e4",
+    "url": "/minesweeper/static/media/windows-xp-bliss.621861ba.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1a1134dea0df84ddfd452e3c5eddf56.js"
+  "/minesweeper/precache-manifest.ba7242b469c4277f850fb49294e6e3c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minesweeper/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
