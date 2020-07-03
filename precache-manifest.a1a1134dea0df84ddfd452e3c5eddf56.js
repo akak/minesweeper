0494232759c8cb82c31739a5d5f2832c93699118
Avi K <aviavigayl@gmail.com>
2020-07-03 17:03:24 +0300

    gh pages

---
+++ precache-manifest.a1a1134dea0df84ddfd452e3c5eddf56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3b7c901edadc7dae24cb163d3a0025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ab67c54cd74e725f148",
+    "url": "/static/css/main.06ecf74c.chunk.css"
+  },
+  {
+    "revision": "71df3084b409723c6ea8",
+    "url": "/static/js/2.b2540727.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2540727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab67c54cd74e725f148",
+    "url": "/static/js/main.7e50559b.chunk.js"
+  },
+  {
+    "revision": "5bf75b294f1ce371b4bc",
+    "url": "/static/js/runtime-main.c056d1e7.js"
+  },
+  {
+    "revision": "621861ba51dd236623b938cfc21916e4",
+    "url": "/static/media/windows-xp-bliss.621861ba.jpg"
+  }
+]);
